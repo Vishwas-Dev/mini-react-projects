5f07cda482752fc6aa840fcf4d67865c85f99a59
Vishwas-Dev <vishwasgupta795@gmail.com>
2025-09-06 04:12:53 +0530

    Accordian Single Selection Completed

---
+++ Accordion/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Accordion from './components/Accordion'
+
+
+function App() {
+  return(
+   <div>
+    <h1>Accordion</h1>
+   <Accordion/>
+   </div>
+  )
+}
+
+export default App
---
+++ Accordion/src/components/Accordion.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useState } from 'react'
+import data from './data'
+import './Accordion.css';
+
+
+function Accordion() {
+
+    const [selected, setSelected] = useState(null);
+
+    function handleSingleSelection(dataId){
+        console.log(dataId);
+        setSelected(dataId == selected ? null : dataId)
+    };
+    console.log(selected)
+
+    return (
+        <div className='wrapper'>
+            <div className="accordion">
+
+                {
+                    data && data.length > 0 ? <div>
+                        {data.map((dataItem) =>
+                        (<div className='item' >
+                            <div className="title" onClick={ () => handleSingleSelection(dataItem.id)}>
+                                <h3>{dataItem.question}</h3>
+                            <span>+</span>
+                            </div>
+                            {
+                                selected === dataItem.id ? 
+                                <div>{dataItem.answer}</div> 
+                                : null
+                            }
+
+                        </div>
+                        ))}
+                    </div> : (<div> "No Data Here" </div>)
+                }
+
+            </div>
+        </div>
+    )
+}
+
+export default Accordion
