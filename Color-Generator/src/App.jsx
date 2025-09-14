677b6e74bb02dd67c004f03e33e26128bef303ed
Vishwas-Dev <vishwasgupta795@gmail.com>
2025-09-14 21:14:52 +0530

    Random color generator

---
+++ Color-Generator/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import RandomColor from './Components/RandomColor/RandomColor'
+
+
+function App() {
+  
+
+  return (
+   <div>
+    <RandomColor/>
+   </div>
+  )
+}
+
+export default App
